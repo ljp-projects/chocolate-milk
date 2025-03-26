f126d793704cbbe3683112b49328034390a55d71
ljp-projects <131211966+ljp-projects@users.noreply.github.com>
2025-03-26 14:11:02 +1100

    stfu

---
+++ language.js
@@ -0,0 +1,35 @@
+const Template = (...args) => {
+    const self = {
+        args
+    }
+
+    return self
+}
+
+const Instruction = {
+    Log(template) {
+        const self = {
+            template,
+            kind: "log",
+        }
+
+        return self
+    }
+}
+
+const console = document.querySelector("#console")
+
+const interpret = (instruction) {
+    if (!instruction.kind) {
+        return null
+    }
+
+    switch (instruction) {
+        case "log":
+            console.textContent = instruction.template.args.join("")
+    }
+}
+
+interpret(
+    Instruction.Log(Template("Hello, World!"))
+)
